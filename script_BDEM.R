7764f1687ab4c1fd38206f0fd9fe1a49188af612
batistone5 <samuelbatistone@id.uff.br>
2026-09-07 02:18:04 -0300

    script BDEM - SIM - tarefas 1 a 3

--- script_BDEM.R
+++ script_BDEM.R
@@ -51,6 +51,9 @@ names(dados_sim_1)
 # 25: PB, 26: PE, 27: AL, 28: SE, 29: BA, 31: MG, 32: ES, 33: RJ, 35: SP, 41: PR, 42: SC, 43: RS
 # 50: MS, 51: MT, 52: GO, 53: DF
 
+#Selecionando apenas os dados do Rio Grande do Sul
+dados_sim_2 = dados_sim_1[substr(dados_sim_1$CODMUNRES, 1, 2) == "43", ]
+
 # observar abaixo o número de óbitos por UF de residência para certificar-se que seu banco de dados está correto
 # 11:8344      12:3763     13:16799    14:2157      15:38557     16:2995     17:7490
 # 21:34362     22:19187    23:54276    24:21922     25:28041     26:66928    27:20769    28:13516     29:88094
@@ -58,6 +61,8 @@ names(dados_sim_1)
 # 41:74740     42:40270    43:87583
 # 50:16749     51:17535    52:38074    53:12050 
 
+#Verificando o número de óbitos (óbitos esperados = 87583)
+nrow(dados_sim_2)
 
 # Ao terminar a Tarefa 3 commit com a mensagem "script BDEM - SIM - tarefas 1 a 3" e envie para o repositório Projeto_BDEM_2016
 
