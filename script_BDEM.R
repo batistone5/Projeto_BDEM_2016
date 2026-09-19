18406c9ae27332b1fbcb728101ff99b6cd505a3e
batistone5 <samuelbatistone@id.uff.br>
2026-09-19 20:22:29 -0300

    script BDEM - SINASC - tarefas 1 a 5

--- script_BDEM.R
+++ script_BDEM.R
@@ -714,6 +714,61 @@ summary(dados_sinasc_2$PESO)
 # KOTELCHUCK = 9 significa "Não informado"   TPROBSON = 11 significa "Não classificado por falta de informação"
 # Em variáveis quantitativas como IDADEMAE verificar se existem valores como 9999 para NA
 
+# Tarefa 5. Transformar categorias ignoradas/não informadas em NA
+
+# LOCNASC
+dados_sinasc_2$LOCNASC[dados_sinasc_2$LOCNASC == "9"] = NA
+
+# ESTCIVMAE
+dados_sinasc_2$ESTCIVMAE[dados_sinasc_2$ESTCIVMAE == "9"] = NA
+
+# GESTACAO
+dados_sinasc_2$GESTACAO[dados_sinasc_2$GESTACAO == "9"] = NA
+
+# GRAVIDEZ
+dados_sinasc_2$GRAVIDEZ[dados_sinasc_2$GRAVIDEZ == "9"] = NA
+
+# PARTO
+dados_sinasc_2$PARTO[dados_sinasc_2$PARTO == "9"] = NA
+
+# SEXO
+dados_sinasc_2$SEXO[dados_sinasc_2$SEXO == "0"] = NA
+
+# RACACOR
+dados_sinasc_2$RACACOR[dados_sinasc_2$RACACOR == "9"] = NA
+
+# IDANOMAL
+dados_sinasc_2$IDANOMAL[dados_sinasc_2$IDANOMAL == "9"] = NA
+
+# ESCMAE2010
+dados_sinasc_2$ESCMAE2010[dados_sinasc_2$ESCMAE2010 == "9"] = NA
+
+# RACACORMAE
+dados_sinasc_2$RACACORMAE[dados_sinasc_2$RACACORMAE == "9"] = NA
+
+# SEMAGESTAC
+dados_sinasc_2$SEMAGESTAC[dados_sinasc_2$SEMAGESTAC == "99"] = NA
+
+# TPAPRESENT
+dados_sinasc_2$TPAPRESENT[dados_sinasc_2$TPAPRESENT == "9"] = NA
+
+# TPROBSON
+dados_sinasc_2$TPROBSON[dados_sinasc_2$TPROBSON == "11"] = NA
+
+# PARIDADE
+dados_sinasc_2$PARIDADE[dados_sinasc_2$PARIDADE == "9"] = NA
+
+# KOTELCHUCK
+dados_sinasc_2$KOTELCHUCK[dados_sinasc_2$KOTELCHUCK == "9"] = NA
+
+# IDADEMAE
+dados_sinasc_2$IDADEMAE[dados_sinasc_2$IDADEMAE == "99"] = NA
+
+# APGAR5
+dados_sinasc_2$APGAR5[dados_sinasc_2$APGAR5 == "99"] = NA
+
+# PESO
+dados_sinasc_2$PESO[dados_sinasc_2$PESO == "9999"] = NA
 
 # Ao terminar a Tarefa 5 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 5" e envie para o repositório Projeto_BDEM_2016
 
