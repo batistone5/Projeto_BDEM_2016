4fc4961ef0f278bf7956fa50fb28661a7e0dfc49
batistone5 <samuelbatistone@id.uff.br>
2026-09-20 22:48:17 -0300

    script BDEM - SINASC - tarefas 1 a 7

--- script_BDEM.R
+++ script_BDEM.R
@@ -914,6 +914,56 @@ dados_sinasc_2$KOTELCHUCK = factor(dados_sinasc_2$KOTELCHUCK,
 # nova variável: dados_sinasc_2$ESTCIV: Sem companheiro: ESTCIVMAE 1, 3 ou 4, Com companheiro: ESTCIVMAE 2 ou 5
 # Ao categorizar as variáveis, garantir que sejam transformadas em tipo fator
 
+# F_PESO
+dados_sinasc_2$F_PESO = cut(
+  dados_sinasc_2$PESO,
+  breaks = c(-Inf, 2499, 3999, Inf),
+  labels = c("Baixo peso", "Peso normal", "Macrossomia"),
+  right = TRUE
+)
+
+# F_IDADE
+dados_sinasc_2$F_IDADE = cut(
+  dados_sinasc_2$IDADEMAE,
+  breaks = c(-Inf, 14, 19, 24, 29, 34, 39, 44, 49, Inf),
+  labels = c("<15", "15-19", "20-24", "25-29", "30-34",
+             "35-39", "40-44", "45-49", "50+"),
+  right = TRUE
+)
+
+# F_APGAR5
+dados_sinasc_2$F_APGAR5 = cut(
+  dados_sinasc_2$APGAR5,
+  breaks = c(-Inf, 6, Inf),
+  labels = c("Baixo", "Normal"),
+  right = TRUE
+)
+
+# PEREG
+dados_sinasc_2$PEREG = factor(
+  ifelse(
+    dados_sinasc_2$CODMUNNASC == dados_sinasc_2$CODMUNRES,
+    "Não",
+    "Sim"
+  ),
+  levels = c("Não", "Sim")
+)
+
+# ESTCIV
+dados_sinasc_2$ESTCIV = factor(
+  ifelse(
+    dados_sinasc_2$ESTCIVMAE %in% c(1, 3, 4),
+    "Sem companheiro",
+    ifelse(
+      dados_sinasc_2$ESTCIVMAE %in% c(2, 5),
+      "Com companheiro",
+      NA
+    )
+  ),
+  levels = c("Sem companheiro", "Com companheiro")
+)
+
+
 
 # Ao terminar a Tarefa 7 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 7" e envie para o repositório Projeto_BDEM_2016
 
